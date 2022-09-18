3a11ee5b568675ad71cd73785b3fccff0026add7
Dima Enns <dieter.enns@gmail.com>
2022-09-18 16:02:42 +0200

    Optimizing cycle detection v2

--- Main/ResolutionBuilding/Function/FunctionCycleTracker.cs
+++ Main/ResolutionBuilding/Function/FunctionCycleTracker.cs
@@ -22,20 +22,18 @@ internal class FunctionCycleTracker : IFunctionCycleTracker
     public void DetectCycle()
     {
         foreach (var function in _adjacencyMap.Keys)
-            DetectCycleInner(function, new Stack<FunctionResolutionBuilderHandle>(), new HashSet<object>());
+            DetectCycleInner(function, new HashSet<object>());
 
-        void DetectCycleInner(FunctionResolutionBuilderHandle current, Stack<FunctionResolutionBuilderHandle> stack, HashSet<object> visited)
+        void DetectCycleInner(FunctionResolutionBuilderHandle current, HashSet<object> visited)
         {
             if (visited.Contains(current.Identity))
                 throw new FunctionCycleDieException();
             if (_adjacencyMap.TryGetValue(current, out var neighbors))
             {
-                stack.Push(current);
                 visited.Add(current.Identity);
                 foreach (var neighbor in neighbors)
-                    DetectCycleInner(neighbor, stack, visited);
+                    DetectCycleInner(neighbor, visited);
                 visited.Remove(current.Identity);
-                stack.Pop();
             }
         }
     }

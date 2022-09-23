d7b9f3d8f608fc2e66d01f9aa53b9507ac9996ba
Dima Enns <dieter.enns@gmail.com>
2022-09-23 20:35:56 +0200

    Collection Tests

---
+++ Test/Collection/IReadOnlyCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MrMeeseeks.DIE.Configuration.Attributes;
+using Xunit;
+
+namespace MrMeeseeks.DIE.Test.Collection.IReadOnlyCollection;
+
+internal interface IInterface {}
+
+internal class ClassA : IInterface {}
+
+internal class ClassB : IInterface {}
+
+internal class ClassC : IInterface {}
+
+[CreateFunction(typeof(IReadOnlyCollection<IInterface>), "Create")]
+internal sealed partial class Container {}
+
+public class Tests
+{
+    [Fact]
+    public async ValueTask Test()
+    {
+        await using var container = new Container();
+        var collection = container.Create();
+        Assert.Equal(3, collection.Count);
+    }
+}

5804a47382f0ff548647290ac960a570d486ebdd
Dima Enns <dieter.enns@gmail.com>
2022-09-23 20:35:56 +0200

    Collection Tests

---
+++ Test/Collection/Array.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using MrMeeseeks.DIE.Configuration.Attributes;
+using Xunit;
+
+namespace MrMeeseeks.DIE.Test.Collection.Array;
+
+internal interface IInterface {}
+
+internal class ClassA : IInterface {}
+
+internal class ClassB : IInterface {}
+
+internal class ClassC : IInterface {}
+
+[CreateFunction(typeof(IInterface[]), "Create")]
+internal sealed partial class Container {}
+
+public class Tests
+{
+    [Fact]
+    public async ValueTask Test()
+    {
+        await using var container = new Container();
+        var collection = container.Create();
+        Assert.Equal(3, collection.Length);
+    }
+}

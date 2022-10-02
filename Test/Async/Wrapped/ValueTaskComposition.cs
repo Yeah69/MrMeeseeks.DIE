3259221813c6816c2a20eb9e1673f3306ad31e28
Dima Enns <dieter.enns@gmail.com>
2022-10-02 10:38:37 +0200

    Fixing remaining potential false positive tests due to ValueTask-bug of xUnit

--- Test/Async/Wrapped/ValueTaskComposition.cs
+++ Test/Async/Wrapped/ValueTaskComposition.cs
@@ -75,7 +75,7 @@ internal sealed partial class Container
 public class Tests
 {
     [Fact]
-    public async ValueTask Test()
+    public async Task Test()
     {
         await using var container = new Container();
         var instance = await container.Create().ConfigureAwait(false);

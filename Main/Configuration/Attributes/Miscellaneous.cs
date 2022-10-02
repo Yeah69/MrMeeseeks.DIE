ca629005e05642856c6486b4d75473ee5b2cb91f
Dima Enns <dieter.enns@gmail.com>
2022-10-02 14:17:20 +0200

    Xml Doc for public attributes

--- Main/Configuration/Attributes/Miscellaneous.cs
+++ Main/Configuration/Attributes/Miscellaneous.cs
@@ -1,6 +1,10 @@
 // ReSharper disable UnusedParameter.Local
 namespace MrMeeseeks.DIE.Configuration.Attributes;
 
+/// <summary>
+/// Marks a custom (transient) scope.
+/// See https://die.mrmeeseeks.dev/scoping/
+/// </summary>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 public class CustomScopeForRootTypesAttribute : Attribute
 {
@@ -9,6 +13,10 @@ public class CustomScopeForRootTypesAttribute : Attribute
     }
 }
 
+/// <summary>
+/// Marks an user-defined constructor parameters injection method.
+/// See https://die.mrmeeseeks.dev/user-defined-elements/
+/// </summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 public class UserDefinedConstructorParametersInjectionAttribute : Attribute
 {
@@ -17,6 +25,10 @@ public class UserDefinedConstructorParametersInjectionAttribute : Attribute
     }
 }
 
+/// <summary>
+/// Marks an user-defined properties injection method.
+/// See https://die.mrmeeseeks.dev/user-defined-elements/
+/// </summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 public class UserDefinedPropertiesInjectionAttribute : Attribute
 {
@@ -25,6 +37,10 @@ public class UserDefinedPropertiesInjectionAttribute : Attribute
     }
 }
 
+/// <summary>
+/// Marks an user-defined initializer parameters injection method.
+/// See https://die.mrmeeseeks.dev/user-defined-elements/
+/// </summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 public class UserDefinedInitializerParametersInjectionAttribute : Attribute
 {
@@ -33,6 +49,10 @@ public class UserDefinedInitializerParametersInjectionAttribute : Attribute
     }
 }
 
+/// <summary>
+/// Lets you specify which types have an initialization method that should be used during instantiation. Pass the type that owns the initialization method first, then the name of the method. There is one constraint on the method: it must return either nothing (void), a Task, or a ValueTask.
+/// See https://die.mrmeeseeks.dev/configuration/miscellaneous/
+/// </summary>
 [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class, AllowMultiple = true)]
 public class InitializerAttribute : Attribute
 {
@@ -41,6 +61,10 @@ public class InitializerAttribute : Attribute
     }
 }
 
+/// <summary>
+/// Discards an active initializer method definition. Pass the type that owns the initializer method.
+/// See https://die.mrmeeseeks.dev/configuration/miscellaneous/
+/// </summary>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 public class FilterInitializerAttribute : Attribute
 {
@@ -49,6 +73,10 @@ public class FilterInitializerAttribute : Attribute
     }
 }
 
+/// <summary>
+/// Defines a create function that DIE should create in the container. This attribute defines which classes should be interpreted as containers. Pass first the type to be returned by the create function and then the name to be given to the create function.
+/// See https://die.mrmeeseeks.dev/configuration/miscellaneous/
+/// </summary>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 public class CreateFunctionAttribute : Attribute
 {
@@ -57,6 +85,9 @@ public class CreateFunctionAttribute : Attribute
     }
 }
 
+/// <summary>
+/// Intended for internal debugging/testing use only. Will prevent build errors and instead expose the exception.
+/// </summary>
 [AttributeUsage(AttributeTargets.Assembly)]
 public class ErrorDescriptionInsteadOfBuildFailureAttribute : Attribute
 {

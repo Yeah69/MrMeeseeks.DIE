69ab9f87cc1319b4525a31dd942335402067be2f
Dima Enns <dieter.enns@gmail.com>
2023-02-21 21:15:29 +0100

    Replaced TypeKey in UserDefinedElements

--- Main/UserDefinedElements.cs
+++ Main/UserDefinedElements.cs
@@ -30,12 +30,12 @@ internal class EmptyUserDefinedElements : IUserDefinedElements
 
 internal class UserDefinedElements : IUserDefinedElements
 {
-    private readonly IReadOnlyDictionary<TypeKey, IFieldSymbol> _typeToField;
-    private readonly IReadOnlyDictionary<TypeKey, IPropertySymbol> _typeToProperty;
-    private readonly IReadOnlyDictionary<TypeKey, IMethodSymbol> _typeToMethod;
-    private readonly IReadOnlyDictionary<TypeKey, IMethodSymbol> _constructorParametersInjectionMethods;
-    private readonly IReadOnlyDictionary<TypeKey, IMethodSymbol> _propertiesInjectionMethods;
-    private readonly IReadOnlyDictionary<TypeKey, IMethodSymbol> _initializerParametersInjectionMethods;
+    private readonly IReadOnlyDictionary<ITypeSymbol, IFieldSymbol> _typeToField;
+    private readonly IReadOnlyDictionary<ITypeSymbol, IPropertySymbol> _typeToProperty;
+    private readonly IReadOnlyDictionary<ITypeSymbol, IMethodSymbol> _typeToMethod;
+    private readonly IReadOnlyDictionary<INamedTypeSymbol, IMethodSymbol> _constructorParametersInjectionMethods;
+    private readonly IReadOnlyDictionary<INamedTypeSymbol, IMethodSymbol> _propertiesInjectionMethods;
+    private readonly IReadOnlyDictionary<INamedTypeSymbol, IMethodSymbol> _initializerParametersInjectionMethods;
 
     public UserDefinedElements(
         // parameter
@@ -64,7 +64,7 @@ internal class UserDefinedElements : IUserDefinedElements
                     _ => throw new ImpossibleDieException(new Guid("B75E24B2-61A3-4C37-B5A5-C7E6D390279D"))
                 };
                 return GetAsyncUnwrappedType(outerType, wellKnownTypes);
-            }, CustomSymbolEqualityComparer.Default)
+            }, CustomSymbolEqualityComparer.IncludeNullability)
             .Where(g => g.Count() > 1)
             .ToImmutableArray();
 
@@ -80,36 +80,39 @@ internal class UserDefinedElements : IUserDefinedElements
                             "Multiple user-defined factories aren't allowed to have the same type.",
                             ExecutionPhase.Validation));
 
-            _typeToField = new Dictionary<TypeKey, IFieldSymbol>();
+            _typeToField = new Dictionary<ITypeSymbol, IFieldSymbol>(CustomSymbolEqualityComparer.IncludeNullability);
         
-            _typeToProperty = new Dictionary<TypeKey, IPropertySymbol>();
+            _typeToProperty = new Dictionary<ITypeSymbol, IPropertySymbol>(CustomSymbolEqualityComparer.IncludeNullability);
 
-            _typeToMethod = new Dictionary<TypeKey, IMethodSymbol>();
+            _typeToMethod = new Dictionary<ITypeSymbol, IMethodSymbol>(CustomSymbolEqualityComparer.IncludeNullability);
         }
         else
         {
             _typeToField = dieMembers
                 .Where(s => s.Name.StartsWith(Constants.UserDefinedFactory))
                 .OfType<IFieldSymbol>()
-                .ToDictionary(
-                    fs => GetAsyncUnwrappedType(fs.Type, wellKnownTypes).ToTypeKey(), 
-                    fs => fs);
+                .ToDictionary<IFieldSymbol, ITypeSymbol, IFieldSymbol>(
+                    fs => GetAsyncUnwrappedType(fs.Type, wellKnownTypes), 
+                    fs => fs,
+                    CustomSymbolEqualityComparer.IncludeNullability);
         
             _typeToProperty = dieMembers
                 .Where(s => s.Name.StartsWith(Constants.UserDefinedFactory))
                 .Where(s => s is IPropertySymbol { GetMethod: { } })
                 .OfType<IPropertySymbol>()
-                .ToDictionary(
-                    ps => GetAsyncUnwrappedType(ps.Type, wellKnownTypes).ToTypeKey(), 
-                    ps => ps);
+                .ToDictionary<IPropertySymbol, ITypeSymbol, IPropertySymbol>(
+                    ps => GetAsyncUnwrappedType(ps.Type, wellKnownTypes), 
+                    ps => ps,
+                    CustomSymbolEqualityComparer.IncludeNullability);
         
             _typeToMethod = dieMembers
                 .Where(s => s.Name.StartsWith(Constants.UserDefinedFactory))
                 .Where(s => s is IMethodSymbol { ReturnsVoid: false, Arity: 0, IsConditional: false, MethodKind: MethodKind.Ordinary })
                 .OfType<IMethodSymbol>()
-                .ToDictionary(
-                    ms => GetAsyncUnwrappedType(ms.ReturnType, wellKnownTypes).ToTypeKey(), 
-                    ms => ms);
+                .ToDictionary<IMethodSymbol, ITypeSymbol, IMethodSymbol>(
+                    ms => GetAsyncUnwrappedType(ms.ReturnType, wellKnownTypes), 
+                    ms => ms,
+                    CustomSymbolEqualityComparer.IncludeNullability);
         }
 
         AddForDisposal = dieMembers
@@ -147,7 +150,7 @@ internal class UserDefinedElements : IUserDefinedElements
         if (validationErrors.Any())
             throw new ValidationDieException(validationErrors.ToImmutableArray());
 
-        IReadOnlyDictionary<TypeKey, IMethodSymbol> GetInjectionMethods(string prefix, INamedTypeSymbol attributeType)
+        IReadOnlyDictionary<INamedTypeSymbol, IMethodSymbol> GetInjectionMethods(string prefix, INamedTypeSymbol attributeType)
         {
             var injectionMethodCandidates = dieMembers
             .Where(s => s.Name.StartsWith(prefix))
@@ -189,12 +192,15 @@ internal class UserDefinedElements : IUserDefinedElements
                                 "Multiple user-defined custom constructor parameter methods aren't allowed to have the same type that they are based on.",
                                 ExecutionPhase.Validation));
 
-                return new Dictionary<TypeKey, IMethodSymbol>();
+                return new Dictionary<INamedTypeSymbol, IMethodSymbol>(CustomSymbolEqualityComparer.Default);
             }
             
             return injectionMethodCandidates
                 .OfType<(INamedTypeSymbol, IMethodSymbol)>()
-                .ToDictionary(t => t.Item1.ConstructTypeUniqueKey(), t => t.Item2);
+                .ToDictionary<(INamedTypeSymbol, IMethodSymbol), INamedTypeSymbol, IMethodSymbol>(
+                    t => t.Item1,
+                    t => t.Item2,
+                    CustomSymbolEqualityComparer.Default);
         }
         
         static ITypeSymbol GetAsyncUnwrappedType(ITypeSymbol type, WellKnownTypes wellKnownTypes)
@@ -209,22 +215,22 @@ internal class UserDefinedElements : IUserDefinedElements
     }
 
     public IFieldSymbol? GetFactoryFieldFor(ITypeSymbol typeSymbol) => 
-        _typeToField.TryGetValue(typeSymbol.ToTypeKey(), out var ret) ? ret : null;
+        _typeToField.TryGetValue(typeSymbol, out var ret) ? ret : null;
 
     public IPropertySymbol? GetFactoryPropertyFor(ITypeSymbol typeSymbol) => 
-        _typeToProperty.TryGetValue(typeSymbol.ToTypeKey(), out var ret) ? ret : null;
+        _typeToProperty.TryGetValue(typeSymbol, out var ret) ? ret : null;
 
     public IMethodSymbol? GetFactoryMethodFor(ITypeSymbol typeSymbol) => 
-        _typeToMethod.TryGetValue(typeSymbol.ToTypeKey(), out var ret) ? ret : null;
+        _typeToMethod.TryGetValue(typeSymbol, out var ret) ? ret : null;
 
     public IMethodSymbol? AddForDisposal { get; }
     public IMethodSymbol? AddForDisposalAsync { get; }
     public IMethodSymbol? GetConstructorParametersInjectionFor(INamedTypeSymbol type) => 
-        _constructorParametersInjectionMethods.TryGetValue(type.ConstructTypeUniqueKey(), out var ret) ? ret : null;
+        _constructorParametersInjectionMethods.TryGetValue(type, out var ret) ? ret : null;
 
     public IMethodSymbol? GetPropertiesInjectionFor(INamedTypeSymbol type) =>
-        _propertiesInjectionMethods.TryGetValue(type.ConstructTypeUniqueKey(), out var ret) ? ret : null;
+        _propertiesInjectionMethods.TryGetValue(type, out var ret) ? ret : null;
 
     public IMethodSymbol? GetInitializerParametersInjectionFor(INamedTypeSymbol type) => 
-        _initializerParametersInjectionMethods.TryGetValue(type.ConstructTypeUniqueKey(), out var ret) ? ret : null;
+        _initializerParametersInjectionMethods.TryGetValue(type, out var ret) ? ret : null;
 }

902dd60efd0450d4d8aea7881e8b2f2ed9ec9e56
Dima Enns <dieter.enns@gmail.com>
2023-07-15 10:00:39 +0200

    Tests for most recent fix

---
+++ Test/InitializedInstances/AsReturnedType.cs
@@ -0,0 +1,30 @@
+using MrMeeseeks.DIE.Configuration.Attributes;
+using Xunit;
+// ReSharper disable UnusedParameter.Local
+
+// ReSharper disable once CheckNamespace
+namespace MrMeeseeks.DIE.Test.InitializedInstances.AsReturnedType;
+
+internal class Dependency
+{
+}
+
+[InitializedInstances(typeof(Dependency))]
+[CreateFunction(typeof(Dependency), "Create")]
+internal sealed partial class Container
+{
+    private Container() {}
+    
+}
+
+public class Tests
+{
+    [Fact]
+    public void Test()
+    {
+        using var container = Container.DIE_CreateContainer();
+        var referenceA = container.Create();
+        var referenceB = container.Create();
+        Assert.Same(referenceA, referenceB);
+    }
+}
---
+++ Test/InitializedInstances/AsReturnedTypeFromFunc.cs
@@ -0,0 +1,31 @@
+using System;
+using MrMeeseeks.DIE.Configuration.Attributes;
+using Xunit;
+// ReSharper disable UnusedParameter.Local
+
+// ReSharper disable once CheckNamespace
+namespace MrMeeseeks.DIE.Test.InitializedInstances.AsReturnedTypeFromFunc;
+
+internal class Dependency
+{
+}
+
+[InitializedInstances(typeof(Dependency))]
+[CreateFunction(typeof(Func<Dependency>), "Create")]
+internal sealed partial class Container
+{
+    private Container() {}
+    
+}
+
+public class Tests
+{
+    [Fact]
+    public void Test()
+    {
+        using var container = Container.DIE_CreateContainer();
+        var referenceA = container.Create()();
+        var referenceB = container.Create()();
+        Assert.Same(referenceA, referenceB);
+    }
+}

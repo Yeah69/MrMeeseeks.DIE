3fa1ddc3c5163fb6fbe41f7b64f34f8c88cc7aaf
Dima Enns <dieter.enns@gmail.com>
2024-12-10 12:26:27 +0100

    Extract configuration tests

---
+++ Test.Configuration/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System;
+using MrMeeseeks.DIE.Configuration.Attributes;
+using MrMeeseeks.DIE.UserUtility;
+
+[assembly:ContainerInstanceAbstractionAggregation(typeof(IContainerInstance))]
+[assembly:TransientScopeInstanceAbstractionAggregation(typeof(ITransientScopeInstance))]
+[assembly:ScopeInstanceAbstractionAggregation(typeof(IScopeInstance))]
+[assembly:TransientScopeRootAbstractionAggregation(typeof(ITransientScopeRoot))]
+[assembly:ScopeRootAbstractionAggregation(typeof(IScopeRoot))]
+[assembly:TransientAbstractionAggregation(typeof(ITransient))]
+[assembly:SyncTransientAbstractionAggregation(typeof(ISyncTransient))]
+[assembly:AsyncTransientAbstractionAggregation(typeof(IAsyncTransient))]
+[assembly:DecoratorAbstractionAggregation(typeof(IDecorator<>))]
+[assembly:CompositeAbstractionAggregation(typeof(IComposite<>))]
+[assembly:Initializer(typeof(IInitializer), nameof(IInitializer.Initialize))]
+[assembly:Initializer(typeof(ITaskInitializer), nameof(ITaskInitializer.InitializeAsync))]
+[assembly:Initializer(typeof(IValueTaskInitializer), nameof(IValueTaskInitializer.InitializeAsync))]
+
+[assembly:InjectionKeyMapping(typeof(InjectionKeyAttribute))]
+[assembly:DecorationOrdinalMapping(typeof(DecorationOrdinalAttribute))]
+
+[assembly:AllImplementationsAggregation]
+
+[assembly:ErrorDescriptionInsteadOfBuildFailure]
+
+[assembly:InvocationDescriptionMapping(typeof(InvocationDescriptionAttribute))]
+
+[AttributeUsage(AttributeTargets.Interface)]
+internal sealed class InvocationDescriptionAttribute : Attribute;
+
+//[assembly:Analytics(Analytics.ResolutionGraph)]
+
+//[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]

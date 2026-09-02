dc6985effd621f6a62c864241cd17b401933ab7c
Dima Enns <dima.enns@deepl.com>
2026-09-02 10:12:39 +0200

    Fix circular dependency in ContainerLogEnhancer

--- Main/Logging/ILogEnhancer.cs
+++ Main/Logging/ILogEnhancer.cs
@@ -1,7 +1,6 @@
 using MrMeeseeks.DIE.Extensions;
 using MrMeeseeks.DIE.MsContainer;
 using MrMeeseeks.DIE.Nodes.Functions;
-using MrMeeseeks.DIE.Nodes.Ranges;
 
 namespace MrMeeseeks.DIE.Logging;
 
@@ -27,7 +26,7 @@ internal sealed class FunctionLevelLogEnhancerDecorator : ILogEnhancer, IDecorat
         _decoratedEnhancer = decoratedEnhancer;
         _prefix = parentFunction.Select(f => f.ReturnedTypeNameNotWrapped);
     }
-    
+
     public string Enhance(string message) => $"[RT:{_prefix.Value}] {_decoratedEnhancer.Enhance(message)}";
 }
 
@@ -35,20 +34,20 @@ internal sealed class ContainerLevelLogEnhancerDecorator : ILogEnhancer, IDecora
 {
     private readonly ICurrentExecutionPhase _currentExecutionPhase;
     private readonly ILogEnhancer _decoratedEnhancer;
-    private readonly Lazy<string> _containerPart;
+    private readonly string _containerPart;
 
     internal ContainerLevelLogEnhancerDecorator(
-        Lazy<IContainerNode> parentContainer,
+        IContainerInfo containerInfo,
         ICurrentExecutionPhase currentExecutionPhase,
         ILogEnhancer decoratedEnhancer)
     {
         _currentExecutionPhase = currentExecutionPhase;
         _decoratedEnhancer = decoratedEnhancer;
-        _containerPart = parentContainer.Select(c => c.Name);
+        _containerPart = containerInfo.Name;
     }
 
-    public string Enhance(string message) => 
-        $"[P:{_currentExecutionPhase.Value.ToString()}][C:{_containerPart.Value}]{_decoratedEnhancer.Enhance(message)}";
+    public string Enhance(string message) =>
+        $"[P:{_currentExecutionPhase.Value.ToString()}][C:{_containerPart}]{_decoratedEnhancer.Enhance(message)}";
 }
 
 internal sealed class ExecuteLevelLogEnhancerDecorator : ILogEnhancer, IDecorator<ILogEnhancer>
